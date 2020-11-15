d79485622b36c8086a9f7a6c3ff6d13f4c8acc98
Ashish Maharjan <amaharjan1032@gmail.com>
2020-11-15 11:59:39 +0545

    Permissions done

--- DRF/blog_api/tests.py
+++ DRF/blog_api/tests.py
@@ -1,19 +1,51 @@
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIClient
 from blog.models import Post, Category
 from django.contrib.auth.models import User
 
 class PostTests(APITestCase):
+
     def test_view_posts(self):
+        """
+        Ensure we can view all objects.
+        """
         url = reverse('blog_api:listcreate')
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def create_post(self):
-        self.test_category = Category.objects.create(name="django")
-        self.testuser1 = User.objects.create_user(username="test1", password="1234")
-        data = {"title":"new", "author":1, "excerpt":"new", "content":"new"}
+    def test_create_account(self):
+        """
+        Ensure we can create a new Post object and view object.
+        """
+        self.test_category = Category.objects.create(name='django')
+
+        self.testuser1 = User.objects.create_superuser(
+            username='test_user1', password='123456789')
+        self.client.login(username=self.testuser1.username, password='123456789')
+        data = {"title": "new", "author": 1,
+                "excerpt": "new", "content": "new"}
         url = reverse('blog_api:listcreate')
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_post_update(self):
+        client = APIClient()
+        self.test_category = Category.objects.create(name='django')
+        self.testuser1 = User.objects.create_user(
+            username='test_user1', password='123456789')
+        self.testuser2 = User.objects.create_user(
+            username='test_user2', password='123456789')
+        test_post = Post.objects.create(category_id=1, title='Test post', excerpt='Post excerpt', content='Test post content', slug='post-title', author_id=1, status='published')
+
+        client.login(username=self.testuser1.username, password='123456789')
+        url = reverse(('blog_api:detailcreate'), kwargs={'pk':1})
+        response = client.put(url, {
+            "id":1,
+            "title":"New",
+            "author":1,
+            "excerpt":"New",
+            "content":"New",
+            "status":"published"
+        }, format='json')
+        print(response.data)
